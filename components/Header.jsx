483d8cc6dfede3bd6cc76412bdcd50ad9be4c105
jasper.kelder <jasperkelder@gmail.com>
2022-07-28 19:06:25 +0200

    Update page transitions and add visual ISR feedback

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,42 +1,102 @@
 import Link from 'next/link';
+import React, { useCallback, useRef } from 'react';
+import ReactCanvasConfetti from 'react-canvas-confetti';
+
+const canvasStyles = {
+  position: 'fixed',
+  pointerEvents: 'none',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+};
 
 export default function Header() {
+  const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback(instance => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio),
+      });
+  }, []);
+
+  const confetti = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot(0.2, {
+      spread: 60,
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }, [makeShot]);
+
   function revalidate() {
     fetch('/api/revalidate');
+    confetti();
   }
 
   return (
-    <header>
-      <nav className='container'>
-        <h1>
-          <Link href='/'>Home</Link>
-        </h1>
-        <span className='header__description'>
-          On-demand{' '}
-          <a className='accent' onClick={() => revalidate()}>
-            ISR
-          </a>
-          , powered by{' '}
-          <a
-            rel='noopener noreferrer'
-            target='_blank'
-            href='https://nextjs.org/'
-            className='accent'
-          >
-            Next.js
-          </a>{' '}
-          and{' '}
-          <a
-            rel='noopener noreferrer'
-            target='_blank'
-            href='https://directus.io/'
-            className='accent'
-          >
-            Directus
-          </a>
-          .
-        </span>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav className='container'>
+          <h1>
+            <Link href='/'>Home</Link>
+          </h1>
+          <span className='header__description'>
+            On-demand{' '}
+            <a className='accent' onClick={() => revalidate()}>
+              {/* <a className='accent' onClick={confetti}> */}
+              ISR
+            </a>
+            , powered by{' '}
+            <a
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://nextjs.org/'
+              className='accent'
+            >
+              Next.js
+            </a>{' '}
+            and{' '}
+            <a
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://directus.io/'
+              className='accent'
+            >
+              Directus
+            </a>
+            .
+          </span>
+        </nav>
+        <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
+      </header>
+    </>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,8 +3,11 @@ import '../styles/globals.css';
 import '../styles/directus.css';
 import { motion } from 'framer-motion';
 import Header from '../components/Header';
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -12,7 +15,7 @@ function MyApp({ Component, pageProps, router }) {
       </Head>
       <Header></Header>
       <motion.div
-        key={router.route}
+        key={router.asPath}
         initial='hidden'
         animate='visible'
         variants={{
